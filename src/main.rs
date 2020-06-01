32309a9b4327accf451b6b4bcdbbea0e38ad04f2
semyon-dev <semyon-dev@protonmail.com>
2020-06-01 14:25:02 +0300

    feat: read line example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::stdin;
+
+// Пример ввода строки с консоли и конвертация в i32
 fn main() {
-    println!("Hello, world!");
+    let mut string: String = String::new();
+    stdin().read_line(&mut string).expect("Не получилось считать строку!");
+    let number: i32 = string.trim().parse::<i32>().unwrap();
+    println!("{}", number);
 }
